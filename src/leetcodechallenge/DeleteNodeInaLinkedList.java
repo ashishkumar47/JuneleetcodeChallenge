2d42632a054c6747ee2874910499468deeffe8c7
ashishkumar47 <ashishkumar8163022@gmail.com>
2020-06-02 22:41:24 +0530

    June2:Delete Node in a Linked List

---
+++ src/leetcodechallenge/DeleteNodeInaLinkedList.java
@@ -0,0 +1,23 @@
+package leetcodechallenge;
+
+public class DeleteNodeInaLinkedList {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
